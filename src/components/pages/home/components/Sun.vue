ad2c0df8b29d0de7637f407eb095a9442f4fc16f
Unknown <costello.ritt@gmail.com>
2018-03-06 20:21:04 -0600

    add background and title to sun component

--- src/components/pages/home/components/Sun.vue
+++ src/components/pages/home/components/Sun.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="sun-container">
-    Sun component alive!
+  <div class="sun-outer-container">
+    <div class="sun-inner-container">
+      <h1>THE SUN</h1>
+    </div>
   </div>
 </template>
 
@@ -11,6 +13,20 @@ export default {
 </script>
 
 <style scoped>
+  .sun-outer-container {
+    background: center fixed;
+    background-image: url(../../../../assets/images/backgrounds/sun.jpg);
+    background-size: cover;
+    height: 100vh;
+    width: 100vw;
+    display: table-cell;
+    margin-top: 0;
+  }
+
+  .sun-inner-container {
+    padding-top: 60px;
+  }
+
   div {
     color: white;
   }
